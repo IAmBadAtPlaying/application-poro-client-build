f9de4630249ea4e6f24c841318fd9a5234c400a7
iambadatplaying <woelkjulian@gmail.com>
2024-10-27 18:29:42 +0100

    Properly use changed repo names

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,16 +3,16 @@
 # Clone and build frontend
 clear
 echo "Handling Frontend"
-if [ -d "poro-client-frontend" ]; then
+if [ -d "application-poro-client-frontend" ]; then
   echo "Repo already present, will not clone!"
-  cd poro-client-frontend
+  cd application-poro-client-frontend || exit
   git pull origin master
   sleep 2s
 else
   echo "Cloning FE Repository"
-  git clone https://github.com/IAmBadAtPlaying/application-poro-client-frontend.git
+  git clone https://github.com/IAmBadAtPlaying/application-poro-client-frontend
   sleep 2s
-  cd poro-client-frontend
+  cd application-poro-client-frontend || exit
 fi
 if [ -d "out" ]; then
   echo "Directory out exists. Deleting..."
@@ -29,18 +29,18 @@ npm run build
 echo "Frontend done"
 
 # Clone and build backend
-cd /app
+cd /app || exit
 echo "Handling Backend"
-if [ -d "Poro-Client" ]; then
+if [ -d "application-poro-client" ]; then
 	echo "Repo already present, will not clone!"
-	cd Poro-Client
+	cd application-poro-client || exit
 	git pull origin master
 	sleep 2s
 else
 	echo "Cloning BE Repository"
 	git clone https://github.com/IAmBadAtPlaying/application-poro-client.git
 	sleep 2s
-	cd Poro-Client
+	cd application-poro-client || exit
 fi
 # Copy the built frontend to the backend resources folder
 cp -r /app/poro-client-frontend/out/* /app/Poro-Client/src/main/resources/html/
